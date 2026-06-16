e233c8da8c94922a1a3fcb56e87fc0103f3dab54
Phil <philwekullo@gmail.com>
2026-06-16 16:42:23 +0300

    fix 3D model error

--- app/components/Models/ContactModels/Computer.tsx
+++ app/components/Models/ContactModels/Computer.tsx
@@ -6,14 +6,15 @@ Files: computer-optimized.glb [486.38KB] > C:\Users\HP\Desktop\Projects\3d-portf
 
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
+import { Mesh } from 'three'
 
 export function Computer(props:any) {
   const { nodes, materials } = useGLTF('/models/computer-optimized-transformed.glb')
   return (
     <group {...props} dispose={null}>
       <group position={[-4.005, 67.549, 58.539]}>
-        <mesh castShadow receiveShadow geometry={nodes.Cube000_ComputerDesk_0001_1.geometry} material={materials['ComputerDesk.001']} />
-        <mesh castShadow receiveShadow geometry={nodes.Cube000_ComputerDesk_0001_2.geometry} material={materials['FloppyDisk.001']} />
+        <mesh castShadow receiveShadow geometry={(nodes.Cube000_ComputerDesk_0001_1 as Mesh).geometry} material={materials['ComputerDesk.001']} />
+        <mesh castShadow receiveShadow geometry={(nodes.Cube000_ComputerDesk_0001_2 as Mesh).geometry} material={materials['FloppyDisk.001']} />
       </group>
     </group>
   )
